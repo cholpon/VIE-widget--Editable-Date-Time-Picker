48f1df0374d43d6f7317e14d9cf6064397b84594
cholpon <cholpon7@gmail.com>
2012-10-09 13:10:46 +0200

    new pages commit

---
+++ demo.js
@@ -0,0 +1,27 @@
+
+$ (function ()  {
+    var vie = window.vie = new VIE({
+       baseNamespace: 'http://schema.org'
+    });
+
+    vie.loadSchema("http://schema.rdfs.org/all.json", {
+        baseNS : "http://schema.org/",
+        success: function () {
+          vie.use(new vie.RdfaRdfQueryService());
+          vie.load({element: jQuery('#content')}).from('rdfardfquery').execute()
+              .success(function(entities) {
+                  console.log(JSON.stringify(entities));
+                  console.log("We got " + entities.length + " editable objects from the page");
+              })
+              .fail(function (e) {
+                  console.warn("failed to load rdfa entities" + e);
+              });
+            
+          $('#content').vieDatePicker({vie: vie, container: $("#entity-cards")}); 
+        },
+        error: function (e) {
+            console.warn("failed to load schema " + e);
+        }
+    });
+
+});
